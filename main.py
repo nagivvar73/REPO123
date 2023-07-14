9987538e0c46f279c650fad1ffc843fef40a1f40
nagivvar73 <46120370+nagivvar73@users.noreply.github.com>
2023-07-14 19:56:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print_hi('PyCharmm123456789QAWERTXXXXXXXXXXXXXXXXXXXXXX')
     print_hi('PyCharmm123456789QAWERT')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
